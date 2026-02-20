ab16609c542eacbd59b5da9346ca34dc42518bfa
Binyam Tagel <binyam.tagel@gmail.com>
2026-02-20 23:14:02 +0300

    feat: Enhance home page with additional content and styling for prayer examples and explanations.

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,29 @@ import { prayerData } from "./data/prayers";
 export default function Home() {
   return (
     <main className="min-h-screen p-6">
-      <h1 className="text-3xl font-bold text-center mb-10 text-blue-900">
+      <h1 className="text-3xl font-bold text-center mb-6 text-black">
         አብነት ትምህርት ቤት
       </h1>
+
+      <div className="max-w-3xl mx-auto">
+        <p className="text-gray-700 text-lg leading-loose text-center mb-12">
+          <strong className="text-black">ግዕዝ ንባብ ምልክት፦</strong>{" "}
+          <sup className="text-sm">ተ</sup>(ተነሽ)፣{" "}
+          <sup className="text-sm">ቂ</sup>(ወዳቂ)፣{" "}
+          <sup className="text-sm">ጣ</sup>(ተጣይ)፣{" "}
+          <sup className="text-sm">ፍ</sup>(ስያፍ-ተነሽ)፣ - (ተናባቢ)
+          <br />
+          <strong className="text-black">ምሳሌ፦</strong> ሰአሊተ
+          <sup className="text-sm">ተ</sup>(ተነሽ)፣ ለነ
+          <sup className="text-sm">ቂ</sup>(ወዳቂ) ድንግል
+          <sup className="text-sm">ፍ</sup>(ተጣይ)፣ እሰግድፍ
+          <sup className="text-sm">ፍ</sup>(ስያፍ-ተነሽ)፣
+          <br />
+          አኃዜ-ኩሉ<sup className="text-sm">ቂ</sup>(ተናባቢ ወዳቂ)፣ ለዓለመ-ዓለም።
+          <sup className="text-sm">ጣ</sup>(ተናባቢ ተጣይ)፣ ሶበ-ትጸልዩ
+          <sup className="text-sm">ተ</sup>(ተናባቢ ተነሽ)
+        </p>
+      </div>
 
       <div className="grid gap-6 md:grid-cols-2">
         {prayerData.map((cat) => (
